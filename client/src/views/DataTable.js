6e2b22085f08369fb0efeccdf65e8fe1e25270df
Song Wei <1983.song.wei@gmail.com>
2017-12-02 16:40:11 -0500

    beautification

--- client/src/views/DataTable.js
+++ client/src/views/DataTable.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDataSheet from 'react-datasheet';
 // Be sure to include styles at some point, probably during your bootstrapping
 import 'react-datasheet/lib/react-datasheet.css';
 class DataTable extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       grid: [
-        [{value:  1}, {value:  3}],
-        [{value:  2}, {value:  4}]
+        [
+          {
+            value: 1
+          }, {
+            value: 3
+          }
+        ],
+        [
+          {
+            value: 2
+          }, {
+            value: 4
+          }
+        ]
       ]
     }
   }
 
   render() {
-    return (
-      <ReactDataSheet
-        data={this.state.grid}
-        valueRenderer={(cell) => cell.value}
-        onChange={(cell, rowI, colJ, value) =>
-          this.setState({
-            grid: this.state.grid.map((col) =>
-              col.map((rowCell) =>
-                (rowCell == cell) ? ({value: value}) : rowCell
-              )
-            )
-          })
-        }
-      />
-    )
+    return (<ReactDataSheet data={this.state.grid} valueRenderer={(cell) => cell.value} onChange={(cell, rowI, colJ, value) => this.setState({
+        grid: this.state.grid.map((col) => col.map(
+          (rowCell) => (rowCell == cell)
+          ? ({value: value})
+          : rowCell))
+      })
+}/>)
   }
 }
 
