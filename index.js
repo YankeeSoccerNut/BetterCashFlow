44b8541f0862fde8858836031eebe6062cf62c30
Shakhor Smith <shakhorsmith@gmail.com>
2017-12-02 17:32:10 -0500

    US banks component

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const axios = require('axios');
 const app = express();
 
 app.use(bodyParser.json());
@@ -16,11 +18,42 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.get('/', (req,res) => {
-  res.send("Home page")
-})
+const user = () => {
+  const options = {
+    method: 'POST',
+    url: 'http://api119525live.gateway.akana.com:80/user/accounts',
+    headers: {
+      'postman-token': 'f97a0999-45f1-03af-b141-70af03fe754e',
+      'cache-control': 'no-cache'
+    },
+    body: '{\n    "LegalParticipantIdentifier": "913996201744144603"\n}'
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+};
+
+app.get('/', (req, res) => {
+  const options = {
+    method: 'POST',
+    url: 'http://api119525live.gateway.akana.com:80/user/accounts',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: '{"LegalParticipantIdentifier": "913996201744144603"}'
+  };
+
+  request(options, function(error, response, body) {
+    if (error) console.log(error);
+
+    res.json(body);
+  });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
