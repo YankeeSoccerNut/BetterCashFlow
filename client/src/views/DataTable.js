7778d039af6d91599e3b703d0c71beb34e37d55c
Song Wei <1983.song.wei@gmail.com>
2017-12-02 18:43:56 -0500

    data table impl switched

--- client/src/views/DataTable.js
+++ client/src/views/DataTable.js
@@ -1,38 +1,45 @@
 import React, {Component} from 'react';
-import ReactDataSheet from 'react-datasheet';
-// Be sure to include styles at some point, probably during your bootstrapping
-import 'react-datasheet/lib/react-datasheet.css';
-class DataTable extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      grid: [
-        [
-          {
-            value: 1
-          }, {
-            value: 3
-          }
-        ],
-        [
-          {
-            value: 2
-          }, {
-            value: 4
-          }
-        ]
-      ]
-    }
-  }
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    products.push({
+      id: id,
+      name: 'Item name ' + id,
+      price: 2100 + i
+    });
+  }
+}
+
+addProducts(5);
+
+function onAfterDeleteRow(rowKeys, rows) {
+  alert('The rowkey you drop: ' + rowKeys);
+}
+
+const options = {
+  afterDeleteRow: onAfterDeleteRow  // A hook for after droping rows.
+};
+
+// If you want to enable deleteRow, you must enable row selection also.
+const selectRowProp = {
+  mode: 'checkbox'
+};
+
+class DataTable extends Component {
   render() {
-    return (<ReactDataSheet data={this.state.grid} valueRenderer={(cell) => cell.value} onChange={(cell, rowI, colJ, value) => this.setState({
-        grid: this.state.grid.map((col) => col.map(
-          (rowCell) => (rowCell == cell)
-          ? ({value: value})
-          : rowCell))
-      })
-}/>)
+    return (
+      <BootstrapTable data={ products } deleteRow={ true } selectRow={ selectRowProp } options={ options }>
+          <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
   }
 }
 
